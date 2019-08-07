e7b423f8bbc08f89165a6b05c30a50e9b71b6190
THIAGO ALVES DE MELO <thiago5423674@hotmail.com>
2019-08-07 18:56:53 -0300

    Criação das classes base repositorio

---
+++ QuickBuy.Dominio/Contratos/IBaseRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IBaseRepositorio<TEntity> : IDisposable where TEntity : class
+    {
+        void Excluir(int id);
+        void Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        TEntity ObterPorId(int id);
+        IEnumerable ObterTodos();
+
+    }
+}
