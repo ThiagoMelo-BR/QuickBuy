3a11e1c8d9d4ce61d3a9d5dd29083164304a03d9
THIAGO ALVES DE MELO <thiago5423674@hotmail.com>
2019-08-22 14:15:37 -0300

    Alteração na view com Angular

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,27 @@
-﻿
+import { Component } from "@angular/core";
+import { Usuario } from "../../model/usuario";
+
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
+
+export class LoginComponent{
+  public usuario;
+  public usuarioAutenticado: boolean;
+
+  constructor() {
+    this.usuario = new Usuario();
+}
+
+  entrar(): void {
+    if (this.usuario.email == "thiago@123" && this.usuario.senha == "123") {
+      this.usuarioAutenticado = true;
+    }
+    else
+      this.usuarioAutenticado = false;
+  }
+
+}
